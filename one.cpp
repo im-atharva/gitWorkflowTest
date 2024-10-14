25ae791b2ac5f3c2a95b23581904a999ff860c74
Atharva Meshram <atharva.g.meshram@gmail.com>
2024-10-14 19:00:10 +0530

    test7

--- one.cpp
+++ one.cpp
@@ -1,26 +1,11 @@
-#include <iostream>
-#include <vector>
-
-void bubbleSort(std::vector<int>& arr) {
-    int n = arr.size();
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                std::swap(arr[j], arr[j + 1]);
-            }
-        }
-    }
-}
-
-int main() {
-    std::vector<int> arr = {64, 34, 25, 12, 22, 11, 90};
-    bubbleSort(arr);
-
-    std::cout << "Sorted array: ";
-    for (const auto& num : arr) {
-        std::cout << num << " ";
-    }
-    std::cout << std::endl;
-
+#include<iostream>
+using namespace std;
+//dfdfd
+int main(){
+    int n = 10;
+    int sum = 0;
+    for(int i=0; i<=n; i++) sum += i;
+  // the file must be changed bsdk
+    
     return 0;
 }
