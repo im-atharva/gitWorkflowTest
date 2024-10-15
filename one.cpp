15aad5195f1723f7690935ad6da4af01b5711521
Atharva Meshram <atharva.g.meshram@gmail.com>
2024-10-15 10:05:17 +0530

    test utility

--- one.cpp
+++ one.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
-//dfdfd
+//example code to find sum of first 10 integers
 int main(){
     int n = 10;
     int sum = 0;
     for(int i=0; i<=n; i++) sum += i;
-  // the file must be changed bsdk
+  // the file must be changed 
     
     return 0;
 }
