5efda348b8f64e140d93f8a3a09cc0b58fd33ad7
im-atharva <atharva.g.meshram@gmail.com>
2024-11-10 22:39:06 +0530

    Test for Python-1

--- gemin.js
+++ gemin.js
@@ -1,23 +1,26 @@
 import { promises as fs } from "fs";
 import axios from "axios";
+const url = "https://94a4-35-196-20-225.ngrok-free.app/optimize";
 
 async function convertToGreenCode() {
   try {
-    const codeContent = await fs.readFile("one.py", "utf8");
+    const codeContent1 = await fs.readFile("one.py", "utf8");
+    const codeContent2 = await fs.readFile("two.py", "utf8");
 
-    const prompt = `${codeContent}`;
-
-    const response = await axios.post(
-      "https://eb9f-104-199-133-146.ngrok-free.app/optimize",
-      {
-        code: prompt,
-      }
-    );
+    const response = await axios.post(`${url}`, {
+      code: codeContent1,
+    });
+    const response2 = await axios.post(`${url}`, {
+      code: codeContent2,
+    });
 
     const optimizedCode = response.data.optimized_code;
+    const optimizedCode2 = response2.data.optimized_code;
     console.log(response.data.optimized_code);
 
     await fs.writeFile("one.py", optimizedCode, "utf8");
+    await fs.writeFile("two.py", optimizedCode2, "utf8");
+
     console.log("Optimized code written to one.cpp");
   } catch (error) {
     console.error("Error during optimization:", error);
---
+++ two.py
@@ -0,0 +1,5 @@
+def calculate_sum(n):
+    total_sum = 0
+    for i in range(1, n + 1):
+        total_sum += i
+    return total_sum
