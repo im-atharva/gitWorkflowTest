51c8d709fea02da67c68263db63977831ddd7993
im-atharva <atharva.g.meshram@gmail.com>
2024-11-11 11:04:26 +0530

    Test-BE-Project

--- gemin.js
+++ gemin.js
@@ -5,21 +5,21 @@ const url = "https://2add-35-237-190-82.ngrok-free.app/optimize";
 async function convertToGreenCode() {
   try {
     const codeContent1 = await fs.readFile("one.py", "utf8");
-    const codeContent2 = await fs.readFile("two.py", "utf8");
+    // const codeContent2 = await fs.readFile("two.py", "utf8");
 
     const response = await axios.post(`${url}`, {
       code: codeContent1,
     });
-    const response2 = await axios.post(`${url}`, {
-      code: codeContent2,
-    });
+    // const response2 = await axios.post(`${url}`, {
+    //   code: codeContent2,
+    // });
 
     const optimizedCode = response.data.optimized_code;
-    const optimizedCode2 = response2.data.optimized_code;
+    // const optimizedCode2 = response2.data.optimized_code;
     console.log(response.data.optimized_code);
 
     await fs.writeFile("one.py", optimizedCode, "utf8");
-    await fs.writeFile("two.py", optimizedCode2, "utf8");
+    // await fs.writeFile("two.py", optimizedCode2, "utf8");
 
     console.log("Optimized code written to one.cpp");
   } catch (error) {
